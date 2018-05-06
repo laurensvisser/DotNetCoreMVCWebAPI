77954c53ec208c790a3a6bfd2388b345f501d19e
Olivier Sourie <Olivier.Sourie@howest.be>
2018-05-06 11:59:52 +0200

    GET

---
+++ DotNetCoreMVCWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Howest;
+
+namespace DotNetCoreMVCWebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        List<Student> studenten;
+
+        public StudentsController()
+        {
+            studenten= new List<Student>
+            {
+                new Student {Id=23, Naam="Jefke", AfstudeerGraad=Graad.Voldoening },
+                new Student {Id=45, Naam="Marieke", AfstudeerGraad=Graad.Onderscheiding },
+            };
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(studenten);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            Student student = studenten.FirstOrDefault(s => s.Id == id);
+            if (student == null) {
+                return NotFound($"Student met id {id} is niet gevonden");
+            }
+            return Ok(student);
+        }
+    }
+}
---
+++ Howest/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Howest
+{
+    public enum Graad
+    {
+        Voldoening,
+        Onderscheiding,
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public Graad AfstudeerGraad { get; set; }
+
+        public override string ToString()
+        {
+            return $"De student met naam {Naam} is met {AfstudeerGraad} afgestudeerd.";
+        }
+
+    }
+}
